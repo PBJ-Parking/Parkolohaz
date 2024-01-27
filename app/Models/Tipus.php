aaffe65c3f4079160ee57b9b4915b8076d996754
terenyijanos96 <terenyi.janos96@gmail.com>
2024-01-27 19:23:22 +0100

    Tipus model, factory és migrációs fájl létrehozása

--- app/Models/Tipus.php
+++ app/Models/Tipus.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Tipus extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'elnevezes'
+    ];
 }
--- database/migrations/2024_01_26_183230_create_tipuses_table.php
+++ database/migrations/2024_01_26_183230_create_tipuses_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('tipus', function (Blueprint $table) {
             $table->id();
+            $table->string('elnevezes');
             $table->timestamps();
         });
     }
