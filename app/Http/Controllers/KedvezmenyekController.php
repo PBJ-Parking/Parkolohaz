45e9b9365c4cd9cc915b27b208b07f70973e2c60
BotkaB <botikabinaka@gmail.com>
2024-03-06 10:29:29 +0100

    ParkolohelyController, KedvezményekController alapfüggvények

--- app/Http/Controllers/KedvezmenyekController.php
+++ app/Http/Controllers/KedvezmenyekController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kedvezmenyek;
 use Illuminate\Http\Request;
 
 class KedvezmenyekController extends Controller
 {
-    //
+    public function index()
+    {
+        $kedvezmenyek = response()->json(Kedvezmenyek::all());
+        return $kedvezmenyek;
+    }
+
+    public function show($id)
+    {
+        $kedvezmeny = response()->json(Kedvezmenyek::find($id));
+        return $kedvezmeny;
+    }
+
+    public function store(Request $request)
+    {
+        $kedvezmeny = new Kedvezmenyek();
+        $kedvezmeny->megnevezes = $request->megnevezes;
+        $kedvezmeny->hatartol = $request->hatartol;
+        $kedvezmeny->mikortol = $request->mikortol;
+        $kedvezmeny->merteke = $request->merteke;
+        $kedvezmeny->save();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $kedvezmeny = Kedvezmenyek::find($id);
+        $kedvezmeny->megnevezes = $request->megnevezes;
+        $kedvezmeny->hatartol = $request->hatartol;
+        $kedvezmeny->mikortol = $request->mikortol;
+        $kedvezmeny->merteke = $request->merteke;
+        $kedvezmeny->save();
+    }
+    public function destroy($id)
+    {
+        Kedvezmenyek::find($id)->delete();
+    }
+
 }
--- app/Http/Controllers/ParkolohelyController.php
+++ app/Http/Controllers/ParkolohelyController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parkolohely;
 use Illuminate\Http\Request;
 
 class ParkolohelyController extends Controller
 {
-    //
+    public function index()
+    {
+        $parkolohelyek = response()->json(Parkolohely::all());
+        return $parkolohelyek;
+    }
+
+    public function show($id)
+    {
+        $parkolohely = response()->json(Parkolohely::find($id));
+        return $parkolohely;
+    }
+
+    public function store(Request $request)
+    {
+        $parkolohely = new Parkolohely();
+        $parkolohely->hely_tipusa = $request->hely_tipusa;
+        $parkolohely->statusz = $request->statusz;
+        $parkolohely->save();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $parkolohely = Parkolohely::find($id);
+        $parkolohely->hely_tipusa = $request->hely_tipusa;
+        $parkolohely->statusz = $request->statusz;
+        $parkolohely->save();
+    }
+    public function destroy($id)
+    {
+        Parkolohely::find($id)->delete();
+    }
 }
