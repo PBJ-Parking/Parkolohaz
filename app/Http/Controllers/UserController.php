bbd06876e2b6768a0b16885a616411037d8815ca
terenyijanos96 <terenyi.janos96@gmail.com>
2024-03-08 11:09:01 +0100

    UserController hibajavítás

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class FelhasznaloController extends Controller
+class UserController extends Controller
 {
     public function index(){
         $felhasznalok = response()->json(User::all());
