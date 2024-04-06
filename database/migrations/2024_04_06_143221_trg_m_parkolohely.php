b3bd85cc7d9e814b05d1608ddc26e4a1f066363f
BotkaB <botikabinaka@gmail.com>
2024-04-06 16:37:52 +0200

    megszüntetett parkolóhely trigger

---
+++ database/migrations/2024_04_06_143221_trg_m_parkolohely.php
@@ -0,0 +1,52 @@
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+         DB::unprepared(
+             "
+         CREATE TRIGGER trg_m_parkolohely_torlese
+         BEFORE Delete ON parkolohely
+         FOR EACH ROW
+         BEGIN
+             IF OLD.statusz = 'M' THEN
+                 SIGNAL SQLSTATE '45000'
+               
+                     SET MESSAGE_TEXT = 'megszüntetett parkolóhely nem törölhető.';
+             END IF;
+         END;"
+        ); 
+
+        DB::unprepared(
+            "
+        CREATE TRIGGER trg_m_parkolohely_modositasa
+        BEFORE Update ON parkolohely
+        FOR EACH ROW
+        BEGIN
+            IF OLD.statusz = 'M'  THEN
+                SIGNAL SQLSTATE '45000'
+              
+                    SET MESSAGE_TEXT = 'megszuntetett parkolóhely nem módosítható.';
+            END IF;
+        END;"
+       ); 
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared(
+            'drop trigger trg_m_parkolohely_torlese
+            drop trigger trg_m_parkolohely_modositasa'
+        );
+    }
+};
